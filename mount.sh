d36c63974e51d2e2b1b59c5b9ea92fa6ca245058
Ilham Winar Nugroho <62131647+ilhamwinar@users.noreply.github.com>
2024-09-09 17:41:25 +0700

    Add files via upload

---
+++ mount.sh
@@ -0,0 +1,4 @@
+mount -t nfs 175.10.1.12:/home/jmto-streaming/project/hlsservice/ita/video /var/www/html/prola/public/video/
+mount -t nfs 175.10.1.12:/home/jmto-streaming/project/hlsservice/deteksi_motor/video /var/www/html/deteksi_motor/public/hls_vid/
+mount -t nfs 175.10.1.20:/volume1/anpr/ /var/www/html/ita/public/Gambar/
+mount -t nfs 175.10.1.20:/volume1/ita/deteksimotor/ /var/www/html/deteksi_motor/public/deteksi_motor/
