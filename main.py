e5e5ab0b7a72abcd896744ff73343a71b952777f
ilhamwinar <ilhamwinar96@gmail.com>
2024-09-13 15:40:49 +0700

    coba standarisasi API semua ITA

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ from datetime import datetime
 import json
 import mysql.connector
 import subprocess
+import playsound
+import requests
+from requests_toolbelt import MultipartEncoder
+from requests.exceptions import HTTPError
+
 
 #defini direktori    
 current_dir = os.getcwd()
@@ -54,11 +59,10 @@ try:
     MyDBLocalCursor = MyDBLocal.cursor()
 except mysql.connector.Error as error:
     # LOGGER.info(f'Failed connect to local server')
-    write_log_error(lokasi_log, "CANNOT CONNECT TO DATABASE"):
+    write_log_error("CANNOT CONNECT TO DATABASE")
     pass
 
 #-----------------------------------------------------------------------------------------------
-
 
 
 def parse_ita_process(gardu,database_connection):
@@ -265,6 +269,11 @@ app.add_middleware(
     allow_headers=["*"],
     expose_headers=["*"])
 
+@app.get("/get_ping")
+async def ping():
+    return {"status":"200"}
+
+#API UNTUK MENJALANKAN SUARA PADA REMOTE PC
 @app.get("/status_auto")
 async def status_test():
     try:
@@ -275,9 +284,151 @@ async def status_test():
         write_log_error("GAGAL PLAY SOUND VIA PLAYSOUND")
         return {"status":"tidak berhasil mode auto playsound"}
 
-@app.get("/get_ping")
-async def ping():
-    return {"status":"200"}
+#----------------------------------------------------------
+# API MOTOR
+
+## GET DATA CONFIG
+@app.post("/get_motor_config")
+async def get_motor_config(location: str = Form()):  
+   
+    #write_log("log_api",str(location))
+    m = MultipartEncoder(fields={'location': str(location)})
+
+    # Define the URL for the API endpoint
+    url = "http://175.10.1.14:8310/get_motor_config"
+    #response = requests.post(url, data=m,headers={'Content-Type': m.content_type})
+    
+    try:
+        #response = requests.get(url)
+        response = requests.post(url, data=m,headers={'Content-Type': m.content_type})
+        response=response.json()
+        response_code=response["status"]
+
+        if response_code==200:
+            id_cctv= response["id_cctv"]
+            rtsp=response["rtsp"]
+            delay=response["delay"]
+            masking=response["masking"]
+            endpoint=response["endpoint"]
+            model=response["model"]
+            created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            sql_insert = """
+            INSERT INTO motor_config (id_cctv, location, rtsp, delay, masking, endpoint, model, created_at) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                rtsp = %s, delay = %s, masking = %s, endpoint = %s, model = %s;
+            """
+            query_insert=(id_cctv,location,rtsp,delay,masking,endpoint,model,created_at,rtsp,delay,masking,endpoint,model)
+            
+            try:
+                #cursor=cnx.cursor()
+                write_log("log_api","START INSERTING TO MYSQL MOTOR CONFIG")
+                cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED")
+                cursor.execute(sql_insert,query_insert)
+                cnx.commit()
+                write_log("log_api","SUCCESSED INSERTING TO MYSQL MOTOR CONFIG")
+            except Exception as err:
+                #write_log("log_api","NOT INSERTED TO DATABASE MOTOR CONFIG")
+                write_log("log_api",str(err))
+    
+            return response
+        
+        elif response_code==404:
+            return {"status":404}
+        
+        # Check if the response returned a server error (5xx)
+        if response.status_code == 500:
+            write_log("log_api","500 Internal Server Error occurred.")
+            # Get response details for debugging
+            write_log("log_api","Response Content:", response.text)
+            write_log("log_api","Response Headers:", response.headers)
+        else:
+            # If the status code is not 500, raise an error for other bad responses (4xx, 5xx)
+            response.raise_for_status()
+
+    except HTTPError as http_err:
+        write_log("log_api",f"HTTP error occurred: {http_err}")  # Output for HTTP errors other than 500
+        return {"status":500}
+
+    except Exception as err:
+        write_log("log_api",f"An error occurred: {err}")  # Handle any other exceptions
+        return {"status":500}
+
+#----------------------------------------------------------
+# API NTP
+
+## GET DATA CONFIG
+@app.post("/get_ntp_config")
+async def get_ntp_config(location: str = Form()):  
+   
+    #write_log("log_api",str(location))
+    m = MultipartEncoder(fields={'location': str(location)})
+
+    # Define the URL for the API endpoint
+    url = "http://175.10.1.14:8310/get_ntp_config"
+    #response = requests.post(url, data=m,headers={'Content-Type': m.content_type})
+    
+    try:
+        #response = requests.get(url)
+        response = requests.post(url, data=m,headers={'Content-Type': m.content_type})
+        response=response.json()
+        response_code=response["status"]
+        write_log("log_api",str(response_code))
+
+        if response_code==200:
+            id_cctv= response["id_cctv"]
+            rtsp=response["rtsp"]
+            delay=response["delay"]
+            masking=response["masking"]
+            endpoint=response["endpoint"]
+            model=response["model"]
+            created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            sql_insert = """
+            INSERT INTO ntp_config (id_cctv, location, rtsp, delay, masking, endpoint, model, created_at) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                rtsp = %s, delay = %s, masking = %s, endpoint = %s, model = %s;
+            """
+            query_insert=(id_cctv,location,rtsp,delay,masking,endpoint,model,created_at,rtsp,delay,masking,endpoint,model)
+            
+            try:
+                #cursor=cnx.cursor()
+                write_log("log_api","START INSERTING TO MYSQL MOTOR CONFIG")
+                cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED")
+                cursor.execute(sql_insert,query_insert)
+                cnx.commit()
+                write_log("log_api","SUCCESSED INSERTING TO MYSQL MOTOR CONFIG")
+            except Exception as err:
+                #write_log("log_api","NOT INSERTED TO DATABASE MOTOR CONFIG")
+                write_log("log_api",str(err))
+    
+            return response
+        
+        elif response_code==404:
+            return {"status":404}
+        
+        # Check if the response returned a server error (5xx)
+        if response.status_code == 500:
+            write_log("log_api","500 Internal Server Error occurred.")
+            # Get response details for debugging
+            write_log("log_api","Response Content:", response.text)
+            write_log("log_api","Response Headers:", response.headers)
+        else:
+            # If the status code is not 500, raise an error for other bad responses (4xx, 5xx)
+            response.raise_for_status()
+
+    except HTTPError as http_err:
+        write_log("log_api",f"HTTP error occurred: {http_err}")  # Output for HTTP errors other than 500
+        return {"status":500}
+
+    except Exception as err:
+        write_log("log_api",f"An error occurred: {err}")  # Handle any other exceptions
+        return {"status":500}
+
+#----------------------------------------------------------
+# API GRAFANA ACTIVE ITA
 
 @app.post("/db_service")
 async def db_service(request: Request):
@@ -333,4 +484,4 @@ async def pingserver(request: Request):
     return {"status":status2}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8100,log_level="info",reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8200,log_level="info",reload=True)
